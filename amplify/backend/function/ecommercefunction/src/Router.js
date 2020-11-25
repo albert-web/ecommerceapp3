bbf57c0f058194a5dd3a4b0a6a9c439bff8b7ae0
albertmensah <DMensah@madisoncollege.edu>
2020-11-25 13:39:54 -0600

    Create Router.js

---
+++ amplify/backend/function/ecommercefunction/src/Router.js
@@ -0,0 +1,42 @@
+import React, {
+    useState
+    , useEffect
+} from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+import Nav from './Nav';
+import Admin from './Admin';
+import Main from './Main';
+import Profile from './Profile';
+
+export default function Router() {
+  const [current, setCurrent] = useState('home')
+  useEffect(
+      () => {
+
+    setRoute()
+    window.addEventListener('hashchange', setRoute)
+    return () =>  window.removeEventListener('hashchange', setRoute)
+  }
+  , []
+  );
+  const setRoute = () => {
+      //Get the location from the windown object.
+    const location = window.location.href.split('/');
+    // We only care about the last part of the windown location... Get the last string in the location array.
+    const pathname = location[location.length-1]
+    console.log('pathname: ', pathname);
+    setCurrent(pathname ? pathname : 'home');
+  }
+  return (
+    <HashRouter>
+      <Nav current={current} />
+      <Switch>
+        <Route exact path='/' component={Main} />
+        <Route path='/admin' component={Admin} />
+        <Route path='/profile' component={Profile} />
+        <Route component={Main} />
+      </Switch>
+    </HashRouter>
+  );
+}
